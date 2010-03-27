03f6447d0fb1803ca80213c4108c84faf34c343e
Chas Lemley <chas.lemley@gmail.com>
2010-03-27 12:21:22 -0400

    various css and js work

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,10 +1,30 @@
 $(document).ready(function() {
-   var path = location.pathname.substring(1);
+	activateCurrentLink();
+	searchTermReplacement($('input#q'), "Search the site");
+});
+
+
+function activateCurrentLink() {
+	 var path = location.pathname.substring(1);
+
    if ( path ) {
      active_link = $('.navigation a[href$="' + path + '"]').parents('li').attr('class', 'active').css('position', 'relative');
 		 var left = (active_link.width() / 2) - 7;
 		 active_link.append("<span class='triangle' style='position:absolute; bottom: -7px; left:"+ left + "px;'></span>");
 	}
-	
+}
 
-});
+function searchTermReplacement(input_field, default_text) {	
+	input_field.attr('value', default_text);
+	
+	input_field.focus(function() {
+		if(input_field.attr('value') == default_text)
+			input_field.attr('value', '');
+	});
+	
+	input_field.blur(function() {
+		 if(input_field.attr('value') == "")
+			input_field.attr('value', default_text);
+	});
+}
+
