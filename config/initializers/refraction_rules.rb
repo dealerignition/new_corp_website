b54e989c69f24c5b2d948dad769a8440aa6a8364
Chas Lemley <chas.lemley@gmail.com>
2010-06-16 10:28:54 -0400

    removed extra slash from redirect url

--- config/initializers/refraction_rules.rb
+++ config/initializers/refraction_rules.rb
@@ -1,7 +1,7 @@
 Refraction.configure do |req|
   case req.host
     when 'www.dealerignition.com'
-      req.permanent! "http://dealerignition.com/#{req.path}"
+      req.permanent! "http://dealerignition.com#{req.path}"
     when /([-\w]+\.)?dealerignition\.com/
       # passthrough with no change
     else  # wildcard domains (e.g. pivotalabs.com)
